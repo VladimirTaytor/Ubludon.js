adf96f3bcab911990efaf8fca21902f65d239aee
Vova <sg.volodymyr@gmail.com>
2017-01-12 22:05:06 +0200

    init commit

---
+++ js/src/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  // TODO: render alerts
+  // <div class="alert alert-danger alert-dismissible" role="alert" th:if="${param.error}">
+  //     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  //     Invalid username or password.
+  // </div>
+  // <div class="alert alert-success alert-dismissible" role="alert" th:if="${param.logout}">
+  //     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  //     You have been logged out.
+  // </div>
+});
